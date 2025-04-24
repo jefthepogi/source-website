be74182d16c9a25829bd54f03cfe0823d2f81a67
Rinax <40387061+MyNameIsRinax@users.noreply.github.com>
2025-04-24 20:32:12 +0800

    Update FlexibleRedirect.jsx

--- src/components/FlexibleRedirect.jsx
+++ src/components/FlexibleRedirect.jsx
@@ -4,6 +4,9 @@ import { useLocation } from 'react-router-dom';
 const redirectMap = {
   '/ccsea-month': 'https://docs.google.com/forms/d/e/1FAIpQLSeLeA-bG-kEpSoCKRDQ4dIjh7OskCkeBJ-DP4-m31ZCG1te8A/viewform?usp=preview',
   '/FunDataAn-Webinar': 'https://docs.google.com/forms/d/e/1FAIpQLSehts3J1HqsK88IvBUoa5RaE4Mc299JWuu2hd__Cvn8Jl2qeA/viewform?usp=header',
+  '/FunDataAn-Evaluation': 'https://docs.google.com/forms/d/e/1FAIpQLSdQuCK9V90YYWC3lt4_7PSwBf_MMohQbv7F-AYO2ZC-ejzY8Q/viewform?usp=header',
+  '/Cybersecurity-Webinar': 'https://docs.google.com/forms/d/e/1FAIpQLScBQ13y73-SYUfkMW1LrVE3-Z_ZLAUf4UhDsVvmoTLr9NbWtg/viewform?usp=header',
+  '/Cybersecurity-Evaluation': 'https://docs.google.com/forms/d/e/1FAIpQLSdaq5befV0wx5vHXHmIfC3KMptEl_0zFUMuzE-JOTqNxqgtxQ/viewform?usp=header',
   '/AdvIAS-Webinar' : 'https://docs.google.com/forms/d/e/1FAIpQLSeRaWTlh80wcEJ78-_vfKxS9aDmrKoUsSvjc55YLkcXE4L7XQ/viewform?usp=header',
   '/AdvIAS-Evaluation' : 'https://docs.google.com/forms/d/e/1FAIpQLSdE0-l_vjEFFUmu255C6XAmC0aZt5IM0Dx2EU1BoWdAWmlCfw/viewform?usp=preview',
   // Add more here
