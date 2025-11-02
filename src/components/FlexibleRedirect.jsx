247fdb8a33bea034e8f68f87057168bc40a6148d
Rinax <40387061+MyNameIsRinax@users.noreply.github.com>
2025-11-02 19:02:34 +0800

    Update FlexibleRedirect.jsx

--- src/components/FlexibleRedirect.jsx
+++ src/components/FlexibleRedirect.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const redirectMap = {
-  '/ccsea-month': 'https://docs.google.com/forms/d/e/1FAIpQLSeLeA-bG-kEpSoCKRDQ4dIjh7OskCkeBJ-DP4-m31ZCG1te8A/viewform?usp=preview',
   '/Git-Webinar': 'https://docs.google.com/forms/d/e/1FAIpQLScvtpRas6FJ39SLZFxdO7AgAol9kONQGpMkkWMsE1euuVdMwg/viewform?usp=header',
   '/Git-Evaluation': 'https://docs.google.com/forms/d/e/1FAIpQLSe4-DWFABHVVz_ZRWEeWYWX1HEHPiFt1KgLrWZf2X-QDS5YRA/viewform?usp=header',
   '/UIUX-Webinar': 'https://docs.google.com/forms/d/e/1FAIpQLSfP3_qOiL8P6JZwWlb9UBFzIJfeoi8F1flmZhfpxdF9K0PzGw/viewform?usp=header',
@@ -11,6 +10,8 @@ const redirectMap = {
   '/AWS-Webinar' : 'https://docs.google.com/forms/d/e/1FAIpQLSehYxXvDOPlxepwBYukl60pcqyTcx76Jg7ZVyGZYuDC4u7HgA/viewform?usp=header',
   '/AWS-Evaluation' : 'https://docs.google.com/forms/d/e/1FAIpQLSe9XiC3vx84LHB6VNz0QAvVJFdKmxPvK-A2zTsryx-CTEsllA/viewform?usp=header',
   '/MerchDesignSubmission' : 'https://docs.google.com/forms/d/e/1FAIpQLSdt2EHOksm3TR7S1vJtADrQO74e52esoI35MG0CZ7Kursa6rQ/viewform?usp=header',
+  '/C2C-Webinar' : 'https://docs.google.com/forms/d/e/1FAIpQLSeDeBqxc8mCbJKoHLLTNobQRdc6PLh0LujzWEOs6kpYTs7rFw/viewform?usp=header',
+  '/C2C-Evaluation' : 'https://docs.google.com/forms/d/e/1FAIpQLSeTL8K_7Xrh0KcmFrdgr6eJ9bgOrUx6AK5VtSDcdrUUCV-F8w/viewform?usp=header',
   '/gamedev-files' : 'https://drive.google.com/uc?export=download&id=1qvVm9NUkD0i5bSR7Wob5wLLDqZ4DukDO',
   // Add more here
 };
