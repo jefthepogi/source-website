1bc089a6021e6c9442247c70d1155b5c282fc390
Rinax <gmrinax@gmail.com>
2026-01-23 21:10:33 +0800

    Update FlexibleRedirect.jsx

--- src/components/FlexibleRedirect.jsx
+++ src/components/FlexibleRedirect.jsx
@@ -15,6 +15,7 @@ const redirectMap = {
   '/gamedev-files' : 'https://drive.google.com/uc?export=download&id=1qvVm9NUkD0i5bSR7Wob5wLLDqZ4DukDO',
   '/TB2026-Evaluation' : 'https://docs.google.com/forms/d/e/1FAIpQLSeFEVqtzDBGoC_d84HvP9ekNIptuwK773AO-2O8gUGDx_Gtaw/viewform?usp=header',
   '/ResearchColloquium' : 'https://docs.google.com/forms/d/e/1FAIpQLSdcI84sA1k9kyN1lh64Lt50-ayMadW99eOs4DZZDQH_PqcePg/viewform?usp=header',
+  '/ResearchColloquium-Evaluation' : 'https://docs.google.com/forms/d/e/1FAIpQLSefyzKsGYc1JhoQmsMPIPnukhjkksrfAwjKiyvoYwU7wYk7Ng/viewform?usp=header',
   // Add more here
 };
 
