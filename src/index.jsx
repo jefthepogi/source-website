771117f532dfc7ff3e8785773c150bb7b9f5d7bd
Rinax <gmrinax@gmail.com>
2026-03-28 16:43:07 +0800

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ApolloProvider from './ApolloProvider';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ApolloProvider>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 );
 
